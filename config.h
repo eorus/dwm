3dbcc3b0df5dfe8f6ad594ecfe37289f5b9f892d
Alper Orus <alperor@gmail.com>
2023-03-26 22:20:34 +0300

    rofi & colors

--- config.h
+++ config.h
@@ -44,9 +44,10 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Brave",    NULL,       NULL,       1 << 2,	      0,	      	 -1 }, // brave opening in second tag
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp",     NULL,         NULL,       0,            1,           -1 },
+	{ "Brave",    NULL,         NULL,       1 << 1,	      0,	      	 -1 }, // brave opening in second tag
+	{ "firefox",  NULL,         NULL,       1 << 3,       0,           -1 },
+  { "LibreWolf",  NULL,       NULL,       1 << 3,       0,           -1 },
 };
 
 /* layout(s) */
