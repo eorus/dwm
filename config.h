74ad8f5f1e27f9390ee6f4edc0b201c984ddeccc
Alper Orus <alperor@gmail.com>
2023-04-26 14:14:49 +0300

    incnmaster

--- config.h
+++ config.h
@@ -94,7 +94,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY|ShiftMask,	            XK_i,      incnmaster,     {.i = -1 } },
+	{ MODKEY,         	            XK_udiaeresis,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
