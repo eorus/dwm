d2fca88a73080597475b81dbc09af7bbe71258c1
Alper Orus <alperor@gmail.com>
2024-11-26 17:17:15 +0300

    status

--- config.h
+++ config.h
@@ -11,8 +11,8 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Source Code Pro:size=12:style=Bold:antialias=true:autohint=false" };
-static const char dmenufont[]       = "Source Code Pro:size=12";
+static const char *fonts[]          = { "Source Code Pro:size=10:style=Bold:antialias=true:autohint=false" };
+static const char dmenufont[]       = "Source Code Pro:size=10";
 static const unsigned int baralpha  = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 
